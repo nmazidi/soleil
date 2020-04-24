9ff443e3258a289859d87046a1cb8af6cc9a5b88
Najim Mazidi <nmazidi95@gmail.com>
2020-04-24 12:57:33 +0100

    Geojson parsing of hourly weather data in geojson_test.dart

--- test/geojson_test.dart
+++ test/geojson_test.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 import 'package:geojson/geojson.dart';
 
 void main() {
-  test("met office http get hourly forecast", () async {
+  test("met office http get hourly forecast and parse", () async {
     final clientId = '93a25d7a-1d1c-4f00-a15d-84687f09da70';
     final clientSecret = 'V3bB8xL7wF8uS7aL6mF8jP5jF3kP2kG1gS5mH7pP2bV4bY0fJ7';
     final Map<String, String> requestHeaders = {
@@ -14,11 +14,17 @@ void main() {
     final lat = '51.454514';
     final long = '-2.587910';
     final url =
-        'https://api-metoffice.apiconnect.ibmcloud.com/metoffice/production/v0/forecasts/point/hourly?excludeParameterMetadata=false&includeLocationName=false&latitude=$lat&longitude=$long';
+        'https://api-metoffice.apiconnect.ibmcloud.com/metoffice/production/v0/forecasts/point/hourly?latitude=$lat&longitude=$long';
 
     final res = await http.get(url, headers: requestHeaders);
     if (res.statusCode == 200) {
-      expect(res.body, isNotNull);
+      final test = await _parseHourlyData(res.body);
+      expect(test, isNotNull);
     }
   });
+}
+
+Future<GeoJsonFeatureCollection> _parseHourlyData(String data) async {
+  final features = await featuresFromGeoJson(data);
+  return features;
 }
