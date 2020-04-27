c54cde8edabca3cb55dce16a49e5a5977fcd7beb
Najim Mazidi <nmazidi95@gmail.com>
2020-04-27 15:26:06 +0100

    Added bloc data parsing and storage using a behaviour subject (update whenever coordinates are updated)

--- lib/src/weatherData_bloc.dart
+++ lib/src/weatherData_bloc.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:collection';
+import 'dart:io';
 import 'package:rxdart/rxdart.dart';
 import 'package:http/http.dart' as http;
 import 'timeSeries.dart';
@@ -30,6 +31,29 @@ class WeatherDataBloc {
     // HashMap to store cached data in
     _cachedData = HashMap<int, TimeSeries>();
     // Listen to a change to the requested coordinates and execute
-    _coordinatesController.stream.listen((coordinates) async {});
+    _coordinatesController.stream.listen((coordinates) async {
+      _getWeatherData(coordinates);
+    });
+  }
+
+  Future<void> _getWeatherData(List<double> coordinates) async {
+    final Map<String, String> requestHeaders = {
+      'accept': 'application/json',
+      'x-ibm-client-id': _clientID,
+      'x-ibm-client-secret': _clientSecret
+    };
+    final _url = '${_baseUrl}latitude=${coordinates[0]}&longitude=${coordinates[1]}';
+    final res = await http.get(
+        _url,
+        headers: requestHeaders);
+    if (res.statusCode == 200) {
+      final timeSeriesList = await parseHourlyData(res.body);
+      if (timeSeriesList.isNotEmpty) {
+        final test = deserializeHourlyData(timeSeriesList);
+        _timeSeriesListSubject.add(test);
+      }
+    } else {
+      throw HttpException(res.body);
+    }
   }
 }
