c16c67f419ab4e4f316e6462b2311af059d7d2bb
Najim Mazidi <nmazidi95@gmail.com>
2020-05-02 13:13:26 +0100

    Moved SplashScreen to separate file

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
-import 'dart:async';
 import 'dart:collection';
+import 'package:drizzle_app/splash.dart';
 import 'package:drizzle_app/src/timeSeries.dart';
 import 'package:drizzle_app/src/weatherData_bloc.dart';
+import 'package:drizzle_app/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:geocoder/geocoder.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'widgets.dart';
 
 void main() {
   // Waits for widgets to initialise before getting any assets.
@@ -31,57 +31,6 @@ class MyApp extends StatelessWidget {
         accentColor: Color(0xFF1EB980),
       ),
       home: SplashScreen(bloc: bloc),
-    );
-  }
-}
-
-class SplashScreen extends StatefulWidget {
-  final WeatherDataBloc bloc;
-  SplashScreen({Key key, this.bloc}) : super(key: key);
-  @override
-  _SplashScreenState createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  Future checkCoordsSet() async {
-    // Get shared preferences from disk.
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    // Checks whether coordinates have already been set from a previous session.
-    bool _defaultCoordsSet = (prefs.getBool('default_coords_set') ?? false);
-    if (_defaultCoordsSet) {
-      // Get address from coords using Geocoder.
-      final _location = await Geocoder.local.findAddressesFromCoordinates(
-          Coordinates(
-              prefs.getDouble('default_lat'), prefs.getDouble('default_long')));
-      Navigator.of(context).pushReplacement(
-        MaterialPageRoute(
-          builder: (context) =>
-              Home(bloc: widget.bloc, location: _location.first),
-        ),
-      );
-    } else {
-      Navigator.of(context).pushReplacement(
-        MaterialPageRoute(
-          builder: (context) => FirstTimeIntro(bloc: widget.bloc),
-        ),
-      );
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    Timer(Duration(milliseconds: 500), () {
-      checkCoordsSet();
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Icon(Icons.cloud, size: 50),
-      ),
     );
   }
 }
---
+++ lib/splash.dart
@@ -0,0 +1,57 @@
+import 'dart:async';
+import 'package:drizzle_app/main.dart';
+import 'package:drizzle_app/src/weatherData_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:geocoder/geocoder.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SplashScreen extends StatefulWidget {
+  final WeatherDataBloc bloc;
+  SplashScreen({Key key, this.bloc}) : super(key: key);
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  Future checkCoordsSet() async {
+    // Get shared preferences from disk.
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    // Checks whether coordinates have already been set from a previous session.
+    bool _defaultCoordsSet = (prefs.getBool('default_coords_set') ?? false);
+    if (_defaultCoordsSet) {
+      // Get address from coords using Geocoder.
+      final _location = await Geocoder.local.findAddressesFromCoordinates(
+          Coordinates(
+              prefs.getDouble('default_lat'), prefs.getDouble('default_long')));
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (context) =>
+              Home(bloc: widget.bloc, location: _location.first),
+        ),
+      );
+    } else {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (context) => FirstTimeIntro(bloc: widget.bloc),
+        ),
+      );
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(milliseconds: 500), () {
+      checkCoordsSet();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Icon(Icons.cloud, size: 50),
+      ),
+    );
+  }
+}
