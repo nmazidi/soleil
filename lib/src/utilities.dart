fb740d024041dbbdcd5f1eed3b5d95545e08f0ce
Najim Mazidi <nmazidi95@gmail.com>
2020-05-05 13:15:44 +0100

    Utilities file with shared utility functions: getDataType and getBaseUrl

---
+++ lib/src/utilities.dart
@@ -0,0 +1,16 @@
+import 'data/dataType.dart';
+
+/// The type of data to get from the API
+String getDataType(DataType type) {
+  switch (type) {
+    case DataType.DAILY: return 'daily'; break;
+    case DataType.HOURLY: return 'hourly'; break;
+    case DataType.THREEHOURLY: return 'three-hourly'; break;
+    default: return 'Invalid data type';
+  }
+}
+
+/// Returns the base Met office API url for given frequency of data
+String getBaseUrl(String dataType) {
+  return 'https://api-metoffice.apiconnect.ibmcloud.com/metoffice/production/v0/forecasts/point/$dataType?';
+}
