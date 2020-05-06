a021490e62b2bd64b5a78eedcb3f75808b416f1e
Najim Mazidi <nmazidi95@gmail.com>
2020-05-06 16:41:51 +0100

    Changed significantWeatherCode names to match icons

--- lib/src/weatherCode.dart
+++ lib/src/weatherCode.dart
@@ -2,37 +2,37 @@
 /// a given Significant Weather Code ID
 String weatherCode(int key) {
   switch (key) {
-    case 0  : return 'Clear night'; break;
-    case 1  : return 'Sunny day'; break;
-    case 2  : return 'Partly cloudy (night)'; break;
-    case 3  : return 'wi_day_cloudy'; break;
+    case 0  : return 'wi-night-clear'; break; // Clear night
+    case 1  : return 'wi-day-sunny'; break; // Sunny day
+    case 2  : return 'wi-night-alt-partly-cloudy'; break; // Partly cloudy (night)
+    case 3  : return 'wi_day_cloudy'; break; // Partly cloudly (day)
     case 4  : return 'Not used'; break;
-    case 5  : return 'Mist'; break;
-    case 6  : return 'Fog'; break;
-    case 7  : return 'wi_cloudy'; break;
-    case 8  : return 'wi-day-sunny-overcast'; break;
-    case 9  : return 'Light rain shower (night)'; break;
-    case 10 : return 'Light rain shower (day)'; break;
-    case 11 : return 'Drizzle'; break;
-    case 12 : return 'Light rain'; break;
-    case 13 : return 'Heavy rain shower (night)'; break;
-    case 14 : return 'Heavy rain shower (day)'; break;
-    case 15 : return 'Heavy rain'; break;
-    case 16 : return 'Sleet shower (night)'; break;
-    case 17 : return 'Sleet shower (day)'; break;
-    case 18 : return 'Sleet'; break;
-    case 19 : return 'Hail shower (night)'; break;
-    case 20 : return 'Hail shower (day)'; break;
-    case 21 : return 'Hail'; break;
-    case 22 : return 'Light snow shower (night)'; break;
-    case 23 : return 'Light snow shower (day)'; break;
-    case 24 : return 'Light snow'; break;
-    case 25 : return 'Heavy snow shower (night)'; break;
-    case 26 : return 'Heavy snow shower (day)'; break;
-    case 27 : return 'Heavy snow'; break;
-    case 28 : return 'Thunder shower (night)'; break;
-    case 29 : return 'Thunder shower (day)'; break;
-    case 30 : return 'Thunder'; break;
+    case 5  : return 'wi-day-fog'; break; // Mist
+    case 6  : return 'wi-day-fog'; break; // Fog
+    case 7  : return 'wi_cloudy'; break; // Cloudy
+    case 8  : return 'wi-day-sunny-overcast'; break; // Overcast
+    case 9  : return 'wi-night-alt-showers'; break; // Light rain shower (night)
+    case 10 : return 'wi-day-showers'; break; // Light rain shower (day)
+    case 11 : return 'wi-sprinkle'; break; // Drizzle
+    case 12 : return 'wi-showers'; break; // Light rain
+    case 13 : return 'wi-night-alt-rain'; break; // Heavy rain shower (night)
+    case 14 : return 'wi-day-rain'; break; // Heavy rain shower (day)
+    case 15 : return 'wi-rain'; break; // Heavy rain
+    case 16 : return 'wi-night-alt-sleet'; break; // Sleet shower (night)
+    case 17 : return 'wi-day-sleet'; break; // Sleet shower (day)
+    case 18 : return 'wi-sleet'; break; // Sleet
+    case 19 : return 'wi-night-alt-hail'; break; // Hail shower (night)
+    case 20 : return 'wi-day-hail'; break; // Hail shower (day)
+    case 21 : return 'wi-hail'; break; // Hail
+    case 22 : return 'wi-night-alt-snow'; break; // Light snow shower (night)
+    case 23 : return 'wi-day-snow'; break; // Light snow shower (day)
+    case 24 : return 'wi-snow'; break; // Light snow
+    case 25 : return 'wi-night-alt-snow'; break; // Heavy snow shower (night)
+    case 26 : return 'wi-day-snow'; break; // Heavy snow shower (day)
+    case 27 : return 'wi-snow'; break; // Heavy snow
+    case 28 : return 'wi-night-alt-thunderstorm'; break; // Thunder shower (night)
+    case 29 : return 'wi-day-thunderstorm'; break; // Thunder shower (day)
+    case 30 : return 'wi-thunderstorm'; break; // Thunder
     default: return 'Weather code invalid';
   }
 }
